80ead13a09e109d7f2145428c4e8abe4b0ba4b30
cjdelve <cdelve@students.mchenry.edu>
2017-10-15 20:04:14 -0500

    Update commercial.java

--- src/com/inheritence/java/commercial.java
+++ src/com/inheritence/java/commercial.java
@@ -4,10 +4,19 @@ public class commercial extends Customer {
     private String property_name;
     private Boolean multiple_prop;
 
+    /**
+     * commercial: Takes the name of the commercial property, and if it is
+     * a multiple property location.
+     *
+     * @param property_name Name of the commercial property.
+     * @param multiple_prop If the location has multiple properties.
+     */
     public commercial(String customer_name, String customer_phone,
                       String customer_address, Double square_feet,
                       String property_name, Boolean multiple_prop){
         super(customer_name, customer_phone, customer_address, square_feet);
+
+        // Define variables specific to this class.
             this.property_name = property_name;
             this.multiple_prop = multiple_prop;
         }
@@ -28,16 +37,15 @@ public class commercial extends Customer {
         return multiple_prop;
     }
 
+    /**
+     * calculations: Determines if there will be a discount provided based on
+     * the multiple_prop parameter.
+     */
     public void calculations(){
         // price = sqft * price per sq ft.
         double price = this.square_feet * .005;
 
         // Determine pricing, display values.
-        // Print standard information.
-        // System.out.println("Customer Name: " + this.customer_name);
-        // System.out.println("Customer Phone: " + this.customer_phone);
-        // System.out.println("Customer Address: " + this.customer_address);
-        // System.out.println("Total Square feet: " + this.square_feet);
 
         // Print property name, specific to this child class.
         System.out.println("Property Name: " + this.property_name);
