899fab31b590969c2c73bfe389af0bb6c10a0c5b
cjdelve <cdelve@students.mchenry.edu>
2017-10-15 20:04:42 -0500

    Update residential.java

--- src/com/inheritence/java/residential.java
+++ src/com/inheritence/java/residential.java
@@ -3,6 +3,12 @@ package com.inheritence.java;
 public class residential extends Customer {
     private Boolean senior;
 
+    /**
+     * residential: Takes information specific to residential customers.
+     *
+     * @param senior If the customer is a senior, they will recieve a
+     *               discount
+     */
     public residential(String customer_name, String customer_phone,
                       String customer_address, Double square_feet,
                       Boolean senior){
@@ -22,12 +28,6 @@ public class residential extends Customer {
         // price = sqft * price per sq ft.
         double price = this.square_feet * .006;
         // Determine pricing, display values.
-
-        // Print standard information.
-        // System.out.println("Customer Name: " + this.customer_name);
-        // System.out.println("Customer Phone: " + this.customer_phone);
-        // System.out.println("Customer Address: " + this.customer_address);
-        // System.out.println("Total Square feet: " + this.square_feet);
 
         if(this.senior){
 
